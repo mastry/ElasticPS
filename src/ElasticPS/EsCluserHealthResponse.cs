2ddf5478c5955940379b50f10d491facb685aee4
Jeff Mastry <jeff@mastry.net>
2015-11-16 09:36:42 -0500

    Added Get-EsClusterHealth

---
+++ src/ElasticPS/EsCluserHealthResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticPS
+{
+    
+    public class EsCluserHealthResponse
+    {
+        public string cluster_name { get; set; }
+        public string status { get; set; }
+        public int number_of_nodes { get; set; }
+        public int number_of_data_nodes { get; set; }
+        public int active_primary_shards { get; set; }
+        public int active_shards { get; set; }
+        public int relocating_shards { get; set; }
+        public int unassigned_shards { get; set; }
+        public int delayed_unassigned_shards { get; set; }
+        public int number_of_pending_tasks { get; set; }
+        public int number_of_in_flight_fetch { get; set; }
+        public int task_max_waiting_in_queue_millis { get; set; }
+        public double active_shards_percent_as_number { get; set; }
+    }
+}
---
+++ src/ElasticPS/GetClusterHealth.cs
@@ -0,0 +1,40 @@
+﻿using ElasticPS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ElasticPS
+{
+    [Cmdlet(VerbsCommon.Get, "EsClusterHealth")]
+    public class GetClusterHealth : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Uri { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (!Uri.EndsWith("/"))
+                Uri += "/";
+
+            var request = new EsRequest("GET", $"{Uri}_cluster/health");
+
+            var response = request.Send();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                var result = ser.Deserialize<EsCluserHealthResponse>(response.Content);
+                WriteObject(result);
+            }
+            else
+            {
+                //TODO - Need an EsError type
+                WriteObject(response);
+            }
+        }
+    }
+}
